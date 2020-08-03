b9d5c12924bd04d8fae5bd55ea2361eda2b3a57e
Georgette Class-Peters <georgycp@udel.edu>
2020-08-03 02:04:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,5 @@
 class Dog
    end 
 ### 2. Make 3 dogs in `lib/dog.rb`
+ dog`fido`, `snoopy`, and `lassie`.
 
-Under your `Dog` class definition, create three dogs in local variables, `fido`, `snoopy`, and `lassie`.
-
-### 3. Define a `Person` in `lib/person.rb`
